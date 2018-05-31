08aed4bef5b9beeef9ffb2fdf22402365d468a01
helenosheaa <helenosheaa@gmail.com>
2018-05-31 18:54:27 +0100

    guests have wallets

--- specs/guests_spec.rb
+++ specs/guests_spec.rb
@@ -4,14 +4,19 @@ require_relative("../guests")
 class GuestTest < MiniTest::Test
 
   def setup
-    @guest1 = Guest.new("Helen")
-    @guest2 = Guest.new("Nick")
-    @guest3 = Guest.new("Cat")
-    @guest4 = Guest.new("Michael")
+    @guest1 = Guest.new("Helen", 20)
+    @guest2 = Guest.new("Nick", 55)
+    @guest3 = Guest.new("Cat", 33)
+    @guest4 = Guest.new("Michael", 25)
   end
 
   def test_guest_has_name
     assert_equal("Helen", @guest1.name())
   end
+
+  def test_guest_has_wallet
+    assert_equal(33, @guest3.wallet())
+  end
+  
 
 end
